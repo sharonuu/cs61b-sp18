18e5ccc45ed91f19285eb590e899d2e33dc766aa
sharonuu <sharonxu1030@outlook.com>
2020-05-09 21:25:27 +0800

    submit Planet.java

---
+++ Planet.java
@@ -0,0 +1,32 @@
+class Planet{
+	public double xxPos=1.0;//Its current x position
+	public double yyPos=2.0;//Its current y position
+	public double xxVel=3.0;//Its current velocity in the x direction
+	public double yyVel=4.0;//Its current velocity in the y direction
+	public double mass=5.0;//Its mass
+	public String imgFileName="jupiter.gif";//The name of the file that corresponds to the image that depicts the planet
+  /** initialize an instance of the Planet class
+  *  @param   xP      Its current x position,
+  *  @param   yP      Its current y position,
+  *  @param   xV      Its current velocity in the x direction
+  *  @param   yV      Its current velocity in the y direction,
+  *  @param   m       Its mass,
+  *  @param   img     The name of the file that corresponds to the image that depicts the planet
+  */
+ public Planet(double xP,double yP,double xV,double yV,double m,String img)
+ {
+  
+ }
+ /** take in a Planet object and initialize an identical Planet object
+   * @param    p      a copy of Plant p
+   */
+ public Planet(Planet p)
+  {
+    double xxPos;
+    double yyPos;
+    double xxVel;
+    double yyVel;
+    double mass;
+    String imgFileName;
+  }
+}
